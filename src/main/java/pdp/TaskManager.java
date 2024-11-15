675edebc5d924c76e5c61958e7ecad7fb79eda66
Shohjahon <shohjahonxadjimuratov59@gmail.com>
2024-11-15 11:40:11 +0500

    create task manager class

---
+++ src/main/java/pdp/TaskManager.java
@@ -0,0 +1,9 @@
+package pdp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    List<Employee> employeeList = new ArrayList<>();
+    List<Task> tasks = new ArrayList<>();
+}
